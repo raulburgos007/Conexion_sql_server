1d357258e0591015ef26bd1b6351805ff755ce58
taigrus <raulburgos007@gmail.com>
2018-11-21 22:06:48 -0400

    Consulta a BD y marcado de presencia

---
+++ app/src/main/java/com/example/raulb/conexion_sql_server/Activo.java
@@ -0,0 +1,15 @@
+package com.example.raulb.conexion_sql_server;
+
+public class Activo{
+    public String descripcion;
+    public String responsable;
+    public String edificio;
+    public String piso;
+    public String ambiente;
+    public String fechaRegistro;
+    public String estado;
+    public String observaciones;
+
+
+
+}
--- app/src/main/java/com/example/raulb/conexion_sql_server/MainActivity.java
+++ app/src/main/java/com/example/raulb/conexion_sql_server/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,27 +14,47 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnLeerCodQR;
-    private EditText editTextCodActivo;
+    private Button btnRegistrPresencia;
+    private Button btnObtenerDatos;
+    private TextView textViewCodActivo;
     private TextView textViewDetalles;
+    private String codigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLeerCodQR = (Button) findViewById(R.id.btnLeerQR);
-        editTextCodActivo = (EditText) findViewById(R.id.editTextCodActivo);
+        btnRegistrPresencia = (Button) findViewById(R.id.btnMarcarPresente);
+        btnObtenerDatos = (Button) findViewById(R.id.btnLeerBD);
+        textViewCodActivo = (TextView) findViewById(R.id.textViewCodActivo);
         textViewDetalles = (TextView) findViewById(R.id.textViewDetalles);
         textViewDetalles.setMovementMethod(new ScrollingMovementMethod());
         btnLeerCodQR.setOnClickListener(this);
+        btnRegistrPresencia.setOnClickListener(this);
+        btnRegistrPresencia.setEnabled(false);
+        btnObtenerDatos.setOnClickListener(this);
+        btnObtenerDatos.setEnabled(false);
+
         //}
     }
 
@@ -43,6 +64,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnLeerQR: //si se hace click en el boton se llama a la actividad de lectura de QR
                 Intent intent = new Intent(this, SimpleScannerActivity.class);
                 startActivityForResult(intent,190); //llamada esperando un resultado con el ID 190
+                break;
+            case R.id.btnMarcarPresente:
+                registraPresencia(codigo);
+                break;
+            case R.id.btnLeerBD:
+                showMessageDialog("Detalles de: " + codigo,obtenerDatosActivoBD(codigo),false);
                 break;
         }
     }
@@ -58,9 +85,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (formato.equals("QRCODE")) {
                         //existe resultado y el codigo es QR
                         String result = data.getStringExtra("datos");
-                        String codigo = result.substring(5, 20).trim();
+                        codigo = result.substring(5, 20).trim();
                         String[] resultDividido = result.split(";");
-                        editTextCodActivo.setText(codigo);
+                        textViewCodActivo.setText(codigo);
                         textViewDetalles.setText("");
                         //editTextDetalles.setText(result);
                         textViewDetalles.setText(resultDividido[1].trim() + '\n' +
@@ -68,23 +95,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 resultDividido[5].trim() + '\n' + resultDividido[6].trim() + '\n' + resultDividido[7].trim() + '\n' +
                                 resultDividido[8].trim() + '\n' + resultDividido[9].trim() + '\n' + resultDividido[10].trim());
                         //showMessageDialog("resultado",result,false);
+                        btnRegistrPresencia.setEnabled(true);
+                        btnObtenerDatos.setEnabled(true);
                     } else {
-                        editTextCodActivo.getText().clear();
+                        textViewCodActivo.setText("");
                         textViewDetalles.setText("");
+                        btnRegistrPresencia.setEnabled(false);
+                        btnObtenerDatos.setEnabled(false);
                         showMessageDialog("Error","El código escaneado no es del tipo QR",false);
                     }
                 }
                 if (resultCode == Activity.RESULT_CANCELED) {
                 //No exite resultado
-                    editTextCodActivo.getText().clear();
+                    textViewCodActivo.setText("");
                     textViewDetalles.setText("");
+                    btnRegistrPresencia.setEnabled(false);
+                    btnObtenerDatos.setEnabled(false);
                     showMessageDialog("Error", "No se pudo leer el código QR", false);
                 }
             }
         }
         catch (Exception e){
-            editTextCodActivo.getText().clear();
+            textViewCodActivo.setText("");
             textViewDetalles.setText("");
+            btnRegistrPresencia.setEnabled(false);
+            btnObtenerDatos.setEnabled(false);
             Log.e(TAG, e.getLocalizedMessage());
             showMessageDialog("Error", "Error al leer e interpretar  el código QR\n"+e.getLocalizedMessage(), false);                    }
     }//onActivityResult
@@ -122,5 +157,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    public Connection conexionBD() {
+        Connection conexion=null;
+        try{
+            StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion= DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.4;databaseName=activos_db;user=sa;password=EmdLa1975;");
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            btnRegistrPresencia.setEnabled(false);
+            btnObtenerDatos.setEnabled(false);
+        }
+        return conexion;
+    }
+
+    public void registraPresencia(String codActivo){
+        try{
+            PreparedStatement pst=conexionBD().prepareStatement("update dbo.activo set activo_fecha_registro = CONVERT (date, getdate()) where activo_codigo_generado=?");
+            pst.setString(1,codActivo);
+            pst.executeUpdate();
+            Toast.makeText(getApplicationContext(),"Registro de presencia exitoso",Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            btnRegistrPresencia.setEnabled(false);
+            btnObtenerDatos.setEnabled(false);
+        }
+    }
+
+    public String obtenerDatosActivoBD(String codigoActivo){
+        String datos="Sin datos";
+        try{
+
+            Statement st=conexionBD().createStatement();
+            String cadenaSQL = "Select Descripcion, Responsable, Edificio, Piso, Ambiente, FechaRegistro, Estado, Observaciones from listado_asignaciones where Codigo='" + codigoActivo + "'";
+            ResultSet rs=st.executeQuery(cadenaSQL);
+            while (rs.next()){
+                String descripcionActivo = rs.getString("descripcion") == null ? "Sin descripción" : rs.getString("descripcion").trim();
+                String observacionesActivo = rs.getString("observaciones") == null ? "Sin observaciones" : rs.getString("observaciones").trim();
+                datos= descripcionActivo + "\nResponsable: " + rs.getString("responsable") + "\nEdificio: " + rs.getString("edificio") +
+                        "\nPiso: " + rs.getString("piso") + "\nAmiente: " +  rs.getString("ambiente") + "\nEstado: " +  rs.getString("estado") +
+                        "\nObservaciones: " + observacionesActivo + "\nUltima verificación: " +  rs.getString("fecharegistro");
+            }
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            btnRegistrPresencia.setEnabled(false);
+            btnObtenerDatos.setEnabled(false);
+        }
+        return datos;
     }
 }
